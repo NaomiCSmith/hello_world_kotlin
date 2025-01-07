14c690767de3a5919085fab336c3a896f1af0a7e
Naomi <naomismith97@yahoo.co.uk>
2025-01-07 16:25:11 +0000

    superify function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -50,8 +50,21 @@ fun main() {
     val listOfThings: List<Any> = listOf(3, "Hello", 4)
     println(listOfThings)
 
+    // Write a method called superify which takes a string and adds the word "super" to the start.
+    // So given 'woman' it returns 'superwoman', given 'dog' it returns 'superdog'.
+    //Then use it to create the ultimate feline superhero, like this:
+    //
+    //println(superify(superify(superify(superify("cat")))))
+    //
+    // Should print:
+    //"supersupersupersupercat"
 
-
+    fun superify(name: String): String {
+        return "super${name}"
+    }
+    println(superify("Kat"))
+    println(superify(superify("michael")))
+    println(superify(superify(superify(superify("cat")))))
 
 
 
